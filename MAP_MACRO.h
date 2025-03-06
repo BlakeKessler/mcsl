3ba9c79d37353df20890e9104d7a8b08a175aa6c
BlakeKessler <blakekessler123@gmail.com>
2025-03-06 17:19:25 -0500

    William Swanson's MAP macro

---
+++ MAP_MACRO.h
@@ -0,0 +1,39 @@
+//obtained from [William Swanson's GitHub](https://github.com/swansontec/map-macro/blob/ce95c50f32b3c9fed2f9780948010dccbe2b75a3/map.h) and slightly modified
+
+#ifndef MCSL_MAP_H
+#define MCSL_MAP_H
+
+#define __MCSL_EVAL0(...) __VA_ARGS__
+#define __MCSL_EVAL1(...) __MCSL_EVAL0(__MCSL_EVAL0(__MCSL_EVAL0(__VA_ARGS__)))
+#define __MCSL_EVAL2(...) __MCSL_EVAL1(__MCSL_EVAL1(__MCSL_EVAL1(__VA_ARGS__)))
+#define __MCSL_EVAL3(...) __MCSL_EVAL2(__MCSL_EVAL2(__MCSL_EVAL2(__VA_ARGS__)))
+#define __MCSL_EVAL4(...) __MCSL_EVAL3(__MCSL_EVAL3(__MCSL_EVAL3(__VA_ARGS__)))
+#define __MCSL_EVAL(...)  __MCSL_EVAL4(__MCSL_EVAL4(__MCSL_EVAL4(__VA_ARGS__)))
+
+#define __MCSL_MAP_END(...)
+#define __MCSL_MAP_OUT
+#define __MCSL_MAP_COMMA ,
+
+#define __MCSL_MAP_GET_END2() 0, __MCSL_MAP_END
+#define __MCSL_MAP_GET_END1(...) __MCSL_MAP_GET_END2
+#define __MCSL_MAP_GET_END(...) __MCSL_MAP_GET_END1
+#define __MCSL_MAP_NEXT0(test, next, ...) next __MCSL_MAP_OUT
+#define __MCSL_MAP_NEXT1(test, next) __MCSL_MAP_NEXT0(test, next, 0)
+#define __MCSL_MAP_NEXT(test, next)  __MCSL_MAP_NEXT1(__MCSL_MAP_GET_END test, next)
+
+#define __MCSL_MAP0(f, x, peek, ...) f(x) __MCSL_MAP_NEXT(peek, __MCSL_MAP1)(f, peek, __VA_ARGS__)
+#define __MCSL_MAP1(f, x, peek, ...) f(x) __MCSL_MAP_NEXT(peek, __MCSL_MAP0)(f, peek, __VA_ARGS__)
+
+#define __MCSL_MAP_LIST_NEXT1(test, next) __MCSL_MAP_NEXT0(test, __MCSL_MAP_COMMA next, 0)
+#define __MCSL_MAP_LIST_NEXT(test, next)  __MCSL_MAP_LIST_NEXT1(__MCSL_MAP_GET_END test, next)
+
+#define __MCSL_MAP_LIST0(f, x, peek, ...) f(x) __MCSL_MAP_LIST_NEXT(peek, __MCSL_MAP_LIST1)(f, peek, __VA_ARGS__)
+#define __MCSL_MAP_LIST1(f, x, peek, ...) f(x) __MCSL_MAP_LIST_NEXT(peek, __MCSL_MAP_LIST0)(f, peek, __VA_ARGS__)
+
+//Applies the function macro `f` to each of the remaining parameters
+#define MCSL_MAP(f, ...) __MCSL_EVAL(__MCSL_MAP1(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
+
+//Applies the function macro `f` to each of the remaining parameters and inserts commas between the results
+#define MCSL_MAP_LIST(f, ...) __MCSL_EVAL(__MCSL_MAP_LIST1(f, __VA_ARGS__, ()()(), ()()(), ()()(), 0))
+
+#endif //MCSL_MAP_H_INCLUDE
---
+++ MAP_MACRO_UNDEF.h
@@ -0,0 +1,33 @@
+//undefine William Swanson's MAP and MAP_LIST macros (as well as all helper macros)
+
+#undef MCSL_MAP_H
+
+#undef __MCSL_EVAL0
+#undef __MCSL_EVAL1
+#undef __MCSL_EVAL2
+#undef __MCSL_EVAL3
+#undef __MCSL_EVAL4
+#undef __MCSL_EVAL
+
+#undef __MCSL_MAP_END
+#undef __MCSL_MAP_OUT
+#undef __MCSL_MAP_COMMA
+
+#undef __MCSL_MAP_GET_END2
+#undef __MCSL_MAP_GET_END1
+#undef __MCSL_MAP_GET_END
+#undef __MCSL_MAP_NEXT0
+#undef __MCSL_MAP_NEXT1
+#undef __MCSL_MAP_NEXT
+
+#undef __MCSL_MAP0
+#undef __MCSL_MAP1
+
+#undef __MCSL_MAP_LIST_NEXT1
+#undef __MCSL_MAP_LIST_NEXT
+
+#undef __MCSL_MAP_LIST0
+#undef __MCSL_MAP_LIST1
+
+#undef MCSL_MAP
+#undef MCSL_MAP_LIST
