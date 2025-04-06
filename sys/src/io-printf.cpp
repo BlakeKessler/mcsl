aa704e4a456ae872c0ad92c022b33b2892916525
BlakeKessler <blakekessler123@gmail.com>
2025-04-05 21:19:20 -0400

    [sys] io - use a `using` declaration instead of fully-qualified names (I don't know why this fixed the problem)

--- sys/src/io-printf.cpp
+++ sys/src/io-printf.cpp
@@ -9,6 +9,7 @@
 #include "throw.hpp"
 
 namespace {
+   using namespace mcsl;
    enum FmtVarFields : ubyte {
       NONE = 0,
       MIN_WIDTH = 1_m,
@@ -24,22 +25,22 @@ namespace {
    }
    #pragma GCC diagnostic push
    #pragma GCC diagnostic ignored "-Wunneeded-internal-declaration"
-   mcsl::tuple<char, mcsl::FmtArgs, uint, FmtVarFields> __parseFmtCode(const mcsl::str_slice str) {
+   tuple<char, FmtArgs, uint, FmtVarFields> __parseFmtCode(const str_slice str) {
       char mode = 0;
-      mcsl::FmtArgs args{};
+      FmtArgs args{};
       uint i = 0;
       FmtVarFields flags = NONE;
       bool usedDefaultPrec = true;
       do {
          if (i >= str.size()) {
-            mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("un-terminated format code"));
+            __throw(ErrCode::FS_ERR, FMT("un-terminated format code"));
          }
          switch (str[i]) {
             case 'f': case 'F':
-               args.precision = mcsl::DEFAULT_FLT_PREC;
+               args.precision = DEFAULT_FLT_PREC;
                [[fallthrough]];
                
-            case mcsl::FMT_INTRO: [[fallthrough]];
+            case FMT_INTRO: [[fallthrough]];
 
             case 'r': case 'R':
             case 'b': case 'B':
@@ -56,7 +57,7 @@ namespace {
 
             case '-': args.isLeftJust = true; ++i; break;
             case '+': args.alwaysPrintSign = true; ++i; break;
-            case mcsl::PAD_CHAR: args.padForPosSign = true; ++i; break;
+            case PAD_CHAR: args.padForPosSign = true; ++i; break;
             case '#': args.altMode = true; ++i; break;
             case '0': args.padWithZero = true; ++i; break;
 
@@ -69,7 +70,7 @@ namespace {
                break;
             
             default:
-               mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("invalid format"));
+               __throw(ErrCode::FS_ERR, FMT("invalid format"));
          }
       } while (!mode);
 
@@ -77,42 +78,42 @@ namespace {
       if (mode == -1) {
          //minWidth
          if (i < str.size()) {
-            if (str[i] == mcsl::FMT_VAR_FIELD) {
+            if (str[i] == FMT_VAR_FIELD) {
                flags |= MIN_WIDTH;
                ++i;
             } else {
-               while (i < str.size() && mcsl::is_digit(str[i], 10)) {
+               while (i < str.size() && is_digit(str[i], 10)) {
                   args.minWidth *= 10;
-                  args.minWidth += mcsl::digit_to_uint(str[i]);
+                  args.minWidth += digit_to_uint(str[i]);
                   ++i;
                }
             }
          }
          
          //precision
-         if (i < str.size() && str[i] == mcsl::FMT_PREC_INTRO) {
+         if (i < str.size() && str[i] == FMT_PREC_INTRO) {
             usedDefaultPrec = false;
             ++i;
-            if (i < str.size() && str[i] == mcsl::FMT_VAR_FIELD) {
+            if (i < str.size() && str[i] == FMT_VAR_FIELD) {
                flags |= PRECISION;
             } else {
-               while (i < str.size() && mcsl::is_digit(str[i], 10)) {
+               while (i < str.size() && is_digit(str[i], 10)) {
                   args.precision *= 10;
-                  args.precision += mcsl::digit_to_uint(str[i]);
+                  args.precision += digit_to_uint(str[i]);
                   ++i;
                }
             }
          }
 
          //radix
-         if (i < str.size() && str[i] == mcsl::FMT_RADIX_INTRO) {
+         if (i < str.size() && str[i] == FMT_RADIX_INTRO) {
             ++i;
-            if (i < str.size() && str[i] == mcsl::FMT_VAR_FIELD) {
+            if (i < str.size() && str[i] == FMT_VAR_FIELD) {
                flags |= RADIX;
             }
-            while (i < str.size() && mcsl::is_digit(str[i], 10)) {
+            while (i < str.size() && is_digit(str[i], 10)) {
                args.radix *= 10;
-               args.radix += mcsl::digit_to_uint(str[i]);
+               args.radix += digit_to_uint(str[i]);
                ++i;
             }
          }
@@ -120,7 +121,7 @@ namespace {
          switch (str[i]) {
             case 'f': case 'F':
                if (usedDefaultPrec) {
-                  args.precision = mcsl::DEFAULT_FLT_PREC;
+                  args.precision = DEFAULT_FLT_PREC;
                }
             case 'r': case 'R':
             case 'b': case 'B':
@@ -134,33 +135,33 @@ namespace {
                ++i;
                break;
 
-            case mcsl::FMT_INTRO: 
-               mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("invalid format (%% may not have arguments)"));
+            case FMT_INTRO: 
+               __throw(ErrCode::FS_ERR, FMT("invalid format (%% may not have arguments)"));
             default:
-               mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("invalid format"));
+               __throw(ErrCode::FS_ERR, FMT("invalid format"));
          }
       }
       #pragma GCC diagnostic pop
 
       if (flags & flags) { //warn the user that variable fields are not yet supported
-         mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("WARNING: mcsl::printf does not yet support variable format fields - if are printing variable-length strings, use an mcsl::str_slice"));
+         __throw(ErrCode::FS_ERR, FMT("WARNING: printf does not yet support variable format fields - if are printing variable-length strings, use an str_slice"));
       }
 
       return {mode, args, i, flags};
    }
 
-   uint __printfImpl(mcsl::File& file, const mcsl::str_slice str, uint charsPrinted);
-   uint __printfImpl(mcsl::File& file, const mcsl::str_slice str, uint charsPrinted, const auto& arg0, const auto&... argv);
+   uint __printfImpl(File& file, const str_slice str, uint charsPrinted);
+   uint __printfImpl(File& file, const str_slice str, uint charsPrinted, const auto& arg0, const auto&... argv);
 
-   uint __printfImpl(mcsl::File& file, const mcsl::str_slice str, uint charsPrinted, const auto& arg0, const auto&... argv) {
+   uint __printfImpl(File& file, const str_slice str, uint charsPrinted, const auto& arg0, const auto&... argv) {
       for (uint i = 0; i < str.size(); ++i) {
-         if (str[i] == mcsl::FMT_INTRO) { //found format code
+         if (str[i] == FMT_INTRO) { //found format code
             if (i) {
-               mcsl::write(file, str.slice(i));
+               write(file, str.slice(i));
                charsPrinted += i;
             }
             ++i;
-            assert(str.size() > i, "%% not followed by format code", mcsl::ErrCode::FS_ERR);
+            assert(str.size() > i, "%% not followed by format code", ErrCode::FS_ERR);
             auto [mode, fmtArgs, codeLen, flags] = __parseFmtCode(str.slice(i, str.size()-i));
 
             // if (flags & MIN_WIDTH) {} //!TODO: figure out a good way to do this
@@ -168,14 +169,14 @@ namespace {
             // if (flags & RADIX) {} //!TODO: figure out a good way to do this
 
             i += codeLen;
-            if (mode == mcsl::FMT_INTRO) { //%%
-               mcsl::write(file, mcsl::FMT_INTRO);
+            if (mode == FMT_INTRO) { //%%
+               write(file, FMT_INTRO);
                ++charsPrinted;
                if (i <= str.size()) { //more to print
                   return __printfImpl(file, str.slice(i, str.size() - i), charsPrinted, arg0, argv...); //tail recursion
                }
             } else { //other format code
-               charsPrinted += mcsl::writef(file, arg0, mode, fmtArgs);
+               charsPrinted += writef(file, arg0, mode, fmtArgs);
                if (i <= str.size()) { //more to print
                   return __printfImpl(file, str.slice(i, str.size() - i), charsPrinted, argv...); //tail recursion
                }
@@ -187,25 +188,25 @@ namespace {
       }
       //no format codes in format string
       [[unlikely]];
-      mcsl::write(file, str);
+      write(file, str);
       charsPrinted += str.size();
       return charsPrinted;
    }
    #pragma GCC diagnostic push
    #pragma GCC diagnostic ignored "-Wunneeded-internal-declaration"
-   uint __printfImpl(mcsl::File& file, const mcsl::str_slice str, uint charsPrinted) {
+   uint __printfImpl(File& file, const str_slice str, uint charsPrinted) {
       for (uint i = 0; i < str.size(); ++i) {
-         if (str[i] == mcsl::FMT_INTRO) { //found format code
+         if (str[i] == FMT_INTRO) { //found format code
             [[unlikely]];
-            mcsl::write(file, str.slice(i));
+            write(file, str.slice(i));
             charsPrinted += i;
             ++i;
-            assert(str.size() > i, "%% not followed by format code", mcsl::ErrCode::FS_ERR);
-            if (str[i] == mcsl::FMT_INTRO) { //%%
-               mcsl::write(file, mcsl::FMT_INTRO);
+            assert(str.size() > i, "%% not followed by format code", ErrCode::FS_ERR);
+            if (str[i] == FMT_INTRO) { //%%
+               write(file, FMT_INTRO);
                ++charsPrinted;
             } else { //other format code
-               mcsl::__throw(mcsl::ErrCode::FS_ERR, mcsl::FMT("printf: more consuming format codes than arguments"));
+               __throw(ErrCode::FS_ERR, FMT("printf: more consuming format codes than arguments"));
             }
             ++i;
             if (i < str.size()) { //more to print
@@ -217,14 +218,14 @@ namespace {
       }
       //no format codes in format string
       [[likely]];
-      mcsl::write(file, str);
+      write(file, str);
       charsPrinted += str.size();
       return charsPrinted;
    }
    #pragma GCC diagnostic pop
 };
 
-uint mcsl::File::printf(const mcsl::str_slice fmt, const auto&... argv) {
+uint mcsl::File::printf(const str_slice fmt, const auto&... argv) {
    return __printfImpl(self, fmt, 0, argv...);
 }
 
